83f7374f1cfca063fbbf05fab6a46894b4dc8f6e
haricharan75 <haricharanadepu1@gmail.com>
2026-08-16 08:04:10 +0530

    Update math_ops.py

--- math_ops.py
+++ math_ops.py
@@ -1,6 +1,10 @@
 def add(a, b):
     return a+b
 
+# def subtract(a, b):
+#     # Intentional bug: returning addition instead of subtraction
+#     return a + b
+
+
 def subtract(a, b):
-    # Intentional bug: returning addition instead of subtraction
-    return a + b
+    return a - b
